321755bd52c16e2d2512a5ddc06e349b3fdac214
Yaroslav Starostin <yistarostin@gmail.com>
2021-02-14 00:32:37 +0300

    improve knn_model

--- src/model/src/knn_model.py
+++ src/model/src/knn_model.py
@@ -1,8 +1,13 @@
-import fasttext
-import fasttext.util
+# import fasttext
+# import fasttext.util
+
 # fasttext.util.download_model('en', if_exists='ignore')
-ft = fasttext.load_model('cc.en.300.bin')
-print(ft.get_dimension())
-print(ft.get_nearest_neighbors('hello'))
+# ft = fasttext.load_model('cc.en.300.bin')
+# print(ft.get_dimension())
+# fasttext.util.reduce_model(ft, 100)
+# print(ft.get_dimension())
+# ft.save_model('cc.en.100.bin')
 
-
+from gensim.models.wrappers import FastText
+model = FastText.load_fasttext_format('cc.en.100.bin')
+print(model.most_similar('better'))
